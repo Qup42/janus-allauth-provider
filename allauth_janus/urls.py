3a6894ff4969cdf9a0509f2a5ab06c02558b44f6
Julian Mundhahs <Qup42@users.noreply.github.com>
2023-01-28 18:53:03 +0100

    Implement Remote Logout as additional view

--- allauth_janus/urls.py
+++ allauth_janus/urls.py
@@ -1,6 +1,14 @@
 from allauth.socialaccount.providers.oauth2.urls import default_urlpatterns
+from django.urls import path
 
+from .app_settings import ALLAUTH_JANUS_LOGOUT
 from .provider import JanusProvider
 
 
 urlpatterns = default_urlpatterns(JanusProvider)
+
+if ALLAUTH_JANUS_LOGOUT == "remote_oidc":
+    from allauth_janus.views import oidc_logout
+    urlpatterns += [
+        path("logout/", oidc_logout, name=f"{JanusProvider.id}_logout")
+    ]
--- allauth_janus/views.py
+++ allauth_janus/views.py
@@ -1,12 +1,21 @@
+from urllib.parse import parse_qsl, urlsplit, urlencode, urlunsplit
+
 from allauth.socialaccount import app_settings as allauth_app_settings
 from allauth.socialaccount.providers.openid_connect.views import OpenIDConnectAdapter
-from allauth_janus.app_settings import ALLAUTH_JANUS_PROFILE_URL
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.urls import resolve
+from django.utils.decorators import method_decorator
+
+from allauth_janus.app_settings import ALLAUTH_JANUS_PROFILE_URL, ALLAUTH_JANUS_LOGOUT
 
 import requests
 
 from allauth.socialaccount.providers.oauth2.views import (
     OAuth2CallbackView,
     OAuth2LoginView,
+    OAuth2View,
 )
 from django.conf import settings
 
@@ -44,5 +53,31 @@ class JanusOAuth2Adapter(OpenIDConnectAdapter):
             extra_data)
 
 
+class LogoutView(OAuth2View):
+
+    def build_logout_url(self, app, id_token, request):
+        url = urlsplit(self.adapter.end_session_url)
+        oidc_rp_initiated_logout_params = [("id_token_hint", id_token.jwt_token),
+                                           ("client_id", app.client_id),
+                                           ("post_logout_redirect_uri", request.build_absolute_uri(getattr(settings, "LOGOUT_REDIRECT_URL", "/")))]
+        query = urlencode(parse_qsl(url.query) + oidc_rp_initiated_logout_params)
+        return urlunsplit((url.scheme, url.netloc, url.path, query, url.fragment))
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        app = self.adapter.get_provider().get_app(self.request)
+        id_tokens_user = JWTToken.objects.filter(social_token__app=app, social_token__account__user=request.user)
+        if id_tokens_user.count() == 0 or not self.adapter.end_session_url:
+            # Just logout at the RP.
+            logout(request)
+            return HttpResponseRedirect(redirect_to=resolve(settings.LOGOUT_REDIRECT_URL))
+        elif ALLAUTH_JANUS_LOGOUT == "remote_oidc" and id_tokens_user.count() >= 1:
+            # Logout at RP and OP using OIDC RP-Initiated Logout.
+            id_token = id_tokens_user.first()
+            logout(request)
+            return HttpResponseRedirect(redirect_to=self.build_logout_url(app, id_token, request))
+
+
 oauth2_login = OAuth2LoginView.adapter_view(JanusOAuth2Adapter)
 oauth2_callback = OAuth2CallbackView.adapter_view(JanusOAuth2Adapter)
+oidc_logout = LogoutView.adapter_view(JanusOAuth2Adapter)
